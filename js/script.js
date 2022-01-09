2905a968db24941e6510e3cba5c5261858ceafaa
Kazi Rasel <kazirasel50@gmail.com>
2022-01-09 13:22:45 +0600

    add my resume

--- js/script.js
+++ js/script.js
@@ -14,39 +14,44 @@ $(document).ready(function () {
             $(".scroll-up-btn").removeClass("show");
         }
     });
+
     // slide-up script
     $(".scroll-up-btn").click(function () {
         $("html").animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $("html").css("scrollBehavior", "auto");
     });
+
     $(".navbar .menu li a").click(function () {
         // applying again smooth scroll on menu items click
         $("html").css("scrollBehavior", "smooth");
     });
+
     // toggle menu/navbar script
     $(".menu-btn").click(function () {
         $(".navbar .menu").toggleClass("active");
         $(".menu-btn i").toggleClass("active");
     });
+
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web developer", "YouTuber", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
     });
+
     var typed = new Typed(".typing-2", {
-        strings: ["Web developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web developer", "YouTuber", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
     });
+
     // owl carousel script
     $(".carousel").owlCarousel({
         margin: 20,
         loop: true,
-        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
